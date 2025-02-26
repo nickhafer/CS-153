88df3f19cb1bbca2480d10dc4d549325840f6c29
nickhafer <107949449+nickhafer@users.noreply.github.com>
2025-02-26 13:46:01 -0800

    added prompts

--- tools/recreation.py
+++ tools/recreation.py
@@ -10,6 +10,106 @@ WEATHER_API_BASE = "https://api.open-meteo.com/v1/forecast?current=temperature_2
 
 logger = logging.getLogger("discord")
 
+MISTRAL_MODEL = "mistral-large-latest"
+
+EXTRACT_LOCATION_PROMPT = """
+Is this message explicitly requesting recreation information for a specific city/location?
+If not, return {"location": "none"}.
+
+Otherwise, return the full name of the city in JSON format.
+
+Example:
+Message: Where can I fish in Salt Lake City?
+Response: {“location”: “Salt Lake City, UT”}
+
+Message: What are the closest campgrounds to Bozeman?
+Response: {“location”: “Bozeman, MT”}
+
+Message: Are there hiking trails near Boston?
+Response: {“location”: “Boston, MA”}
+
+Message: Give me the hiking trails in Boulder.
+Response: {“location”: “Boulder, CO”}
+
+Message: I love hiking in sf!
+Response: {“location”: “none”}
+
+Message: Is camping fun in NYC?
+Response: {“location”: “none”}
+"""
+
+EXTRACT_ACTIVITY_PROMPT = """
+Is this message explicitly requesting recreation information for a specific activity?
+If not, return {“ActivityName”: "none"}.
+
+Otherwise, return the full name of the city in JSON format.
+
+Example:
+Message: Where can I fish in Salt Lake City?
+Response: {“ActivityName”: “FISHING”}
+
+Message: What are the closest campgrounds to Bozeman?
+Response: {“ActivityName”: “CAMPING”}
+
+Message: Are there hiking trails near Boston?
+Response: {“ActivityName”: “HIKING”}
+
+Message: Give me the hiking trails in Boulder.
+Response: {“ActivityName”: “HIKING”}
+
+Message: I love hiking in sf!
+Response: {“ActivityName”: “none”}
+
+Message: Is camping fun in NYC?
+Response: {“ActivityName”: “none”}
+"""
+
+EXTRACT_RADIUS_PROMPT = """
+Is this message explicitly requesting recreation information for a specific radius?
+If not, return {“Radius”: “10”}.
+
+Otherwise, return the radius specified. If the value is greater than 50, return 50.
+
+Example:
+Message: Where can I fish within 30 miles of  Salt Lake City?
+Response: {“Radius”: “30”}
+
+Message: What are the closest campgrounds to Bozeman?
+Response: {“Radius”: “10”}
+
+Message: Are there hiking trails within 60 miles of Boston?
+Response: {“Radius”: “60”}
+
+Message: Give me the hiking trails in Boulder.
+Response: {“Radius”: “None”}
+
+Message: I love hiking in sf!
+Response: {“Radius”: “none”}
+
+Message: Is camping fun in NYC?
+Response: {“Radius”: “none”}
+"""
+
+EXTRACT_LIMIT_PROMPT = """
+Is this message explicitly requesting a limit on the number of results to return?
+If not, return {“Limit”: “5”}.
+
+Otherwise, return the limit specified, with a max of ten. 
+
+Example:
+Message: Show me the 3 best places to fish near Salt Lake City.
+Response: {“Limit”: “3”}
+
+Message: What are the closest campgrounds to Bozeman?
+Response: {“Limit”: “5”}
+
+Message: Show me 11 hiking trails within 60 miles of Boston?
+Response: {“Limit”: “10”}
+
+Message: Give me the hiking trails in Boulder.
+Response: {“Limit”: “5”}
+"""
+
 
 def _make_request(url: str):
     headers = {"User-Agent": USER_AGENT, "Accept": "application/geo+json"}
